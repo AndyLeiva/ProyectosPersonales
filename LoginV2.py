7d2024e5627f5f529d45d9e1d46e2b72d336c574
AndyLeiva <andy.leiva.camacho@gmail.com>
2021-06-25 17:45:24 -0600

    Aplicacion de Registro de Usuarios Lista
    Se ha podiddo concretar los objetivos como registar los datos de usuario y contraseña en la tabla usuarios de la base de datos Login.db

--- LoginV2.py
+++ LoginV2.py
@@ -2,54 +2,58 @@
 #Crear inicio de sesión y que compruebe si el usuario y contraseña existe en sqlite
 
 #Importar la libreria sqlite3
-
+from tkinter import *
+from tkinter import Entry
 import sqlite3
 from sqlite3.dbapi2 import Cursor
 
-#conexion = sqlite3.connect("Login.db")
+conexion = sqlite3.connect("Login.db")
 
-#cursor = conexion.cursor()
+cursor = conexion.cursor()
 
 #   Creación de la Tabla
 #cursor.execute("CREATE TABLE IF NOT EXISTS usuarios "\
     #"(usuario VARCHAR(100), contrasena VARCHAR(100))")
 
-#   Funcion de insertar los datos a la tabla usuarios
-#def insertarDatos(usuario,contrasena):    
-    #cursor.execute("INSERT INTO usuarios VALUES (?,?)",(usuario, contrasena))
 
-    #conexion.commit()
+conexion.close()
 
-#insertarDatos(input("USUARIO: "),input("CONTRASEÑA: "))
 
-#conexion.close()
 
 #Crear interfaz gráfica del login
-from tkinter import *
+
 root = Tk()
 root.title("Login")
 root.geometry("400x300")
 root.resizable(False,False)
 
+#   Funcion de insertar los datos a la tabla usuarios
+def registrar():
+    #cursor.execute('INSERT INTO usuarios()nombre values()')
+    #conexion.commit()
+    print("Boton presionado ", valorUsuario.get())
+
+valorUsuario = StringVar()
+valorContrasena = StringVar()
 
 #Crear las etiquetas
 etiquetaUsuario = Label(root, text="Ingrese su usuario: ", font="Arial 12")
 etiquetaUsuario.place(x=10, y=70)
 
-etiquetaContrasena = Label(root, text="Ingrese su usuario: ", font="Arial 12")
+etiquetaContrasena = Label(root, text="Ingrese su Contraseña: ", font="Arial 12")
 etiquetaContrasena.place(x=10, y=150)
 
 #Crear los Entry(Cajas de texto)
-entryUsuario = Entry(root)
-entryUsuario.place(x=150, y=70)
+entryUsuario = Entry(root, textvariable=valorUsuario)
+entryUsuario.place(x=180, y=70)
 
-entryContrasena = Entry(root)
-entryContrasena.place(x=150, y=151)
+entryContrasena = Entry(root, textvariable=valorContrasena)
+entryContrasena.place(x=180, y=151)
 
 
 #Crear Boton para guardar el usuario y contraseña
 
-botonRegistrar = Button(root, text="Registrar datos", font="Arial 12")
+botonRegistrar = Button(root, text="Registrar datos", font="Arial 12", command="registrar")
 botonRegistrar.place(x=150, y=220)
 
 root.mainloop()
---
+++ RegistrarUsuarios.py
@@ -0,0 +1,52 @@
+from tkinter import *
+root = Tk()
+root. title("Registrar Usuarios")
+root.geometry("400x300")
+
+import sqlite3
+from sqlite3.dbapi2 import Cursor
+
+conexion = sqlite3.connect("Login.db")
+#cursor = conexion.cursor()
+#   Creación de la Tabla
+#cursor.execute("CREATE TABLE IF NOT EXISTS usuarios "\
+    #"(usuario VARCHAR(100), contrasena VARCHAR(100))")
+
+def registrar():
+    #Conexion con la Base de datos
+    cursor1 = conexion.cursor()
+    listaDatos = [valorUsuario.get(), valorContrasena.get()]
+    #listaContrasena = [valorContrasena.get()]
+    
+    #Insertar datos ala tabla usuarios en las columnas usuario y contraseña
+    cursor1.execute('INSERT INTO usuarios(usuario, contrasena) values(?,?)', listaDatos)
+    conexion.commit()
+
+    
+    print("Boton presionado ", valorUsuario.get(), valorContrasena.get())
+
+
+
+#Variable de las cajas de texto
+valorUsuario = StringVar()
+valorContrasena = StringVar()
+
+#Etiquetas
+etiquetaUsuario = Label(root, text="Escribe el Usuario: ")
+etiquetaUsuario.place(x=10, y=50)
+
+etiquetaContrasena = Label(root, text="Escribe la contraseña: ")
+etiquetaContrasena.place(x=10, y=100)
+
+#Cajas de texto
+cajaUsuario = Entry(root, textvariable=valorUsuario)
+cajaUsuario.place(x=180, y=50)
+cajaContrasena = Entry(root, textvariable=valorContrasena)
+cajaContrasena.place(x=180, y=100)
+
+botonRegistrar = Button(root, text="Registrar", command=registrar)
+botonRegistrar.place(x=150, y=250)
+
+
+root.mainloop()
+    
