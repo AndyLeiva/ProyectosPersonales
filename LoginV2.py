6855951e2789a568ab713c3e41c8b6f5eb772cd3
AndyLeiva <andy.leiva.camacho@gmail.com>
2021-06-25 11:54:31 -0600

    Creación de la interfaz
    Se ha creado la interfaz sin ninguna funcion, solo tiene agreagdo los objetos como etiquetas, cajas de texto y botones

--- LoginV2.py
+++ LoginV2.py
@@ -6,19 +6,55 @@
 import sqlite3
 from sqlite3.dbapi2 import Cursor
 
-conexion = sqlite3.connect("Login.db")
+#conexion = sqlite3.connect("Login.db")
 
-cursor = conexion.cursor()
+#cursor = conexion.cursor()
 
 #   Creación de la Tabla
 #cursor.execute("CREATE TABLE IF NOT EXISTS usuarios "\
     #"(usuario VARCHAR(100), contrasena VARCHAR(100))")
 
+#   Funcion de insertar los datos a la tabla usuarios
+#def insertarDatos(usuario,contrasena):    
+    #cursor.execute("INSERT INTO usuarios VALUES (?,?)",(usuario, contrasena))
 
-def insertarDatos(usuario,contrasena):    
-    cursor.execute("INSERT INTO usuarios VALUES (?,?)",(usuario, contrasena))
+    #conexion.commit()
 
-    conexion.commit()
-insertarDatos(input("USUARIO: "),input("CONTRASEÑA: "))
+#insertarDatos(input("USUARIO: "),input("CONTRASEÑA: "))
 
-conexion.close()
+#conexion.close()
+
+#Crear interfaz gráfica del login
+from tkinter import *
+root = Tk()
+root.title("Login")
+root.geometry("400x300")
+root.resizable(False,False)
+
+
+#Crear las etiquetas
+etiquetaUsuario = Label(root, text="Ingrese su usuario: ", font="Arial 12")
+etiquetaUsuario.place(x=10, y=70)
+
+etiquetaContrasena = Label(root, text="Ingrese su usuario: ", font="Arial 12")
+etiquetaContrasena.place(x=10, y=150)
+
+#Crear los Entry(Cajas de texto)
+entryUsuario = Entry(root)
+entryUsuario.place(x=150, y=70)
+
+entryContrasena = Entry(root)
+entryContrasena.place(x=150, y=151)
+
+
+#Crear Boton para guardar el usuario y contraseña
+
+botonRegistrar = Button(root, text="Registrar datos", font="Arial 12")
+botonRegistrar.place(x=150, y=220)
+
+root.mainloop()
+
+
+
+
+
