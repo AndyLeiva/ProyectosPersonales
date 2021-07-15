e71e5267c15c6e16c8052bc67cc0a4ab1c7f205c
AndyLeiva <andy.leiva.camacho@gmail.com>
2021-07-14 19:33:46 -0600

    Update calculadora.py

--- Tarea #2 Ciber/calculadora.py
+++ Tarea #2 Ciber/calculadora.py
@@ -33,36 +33,46 @@ class Calculadora:
         
         print("El resultado de la divición es: ", resultado)
         
+continuar = ""        
+while continuar != "1" or continuar != "2" or continuar != "3" or continuar != "4" :        
+    
+    print("BIENVENIDO A LA CALCULADORA, \n1-Sumar\n2-Restar\n3-Multiplicar\n4-División\n5-Salir de la calculadora\n") 
+
+
+    opcion = input("Ingrese el numero de su opcion que aparece en el menú: ")
+
+
+    while not opcion.isdigit():
+        print("¡ERROR!\n FAVOR INGRESE UN NUMERO ENTERO\n")
+        print("BIENVENDO A LA CALCULADORA, \n1-Sumar\n2-Restar\n3-Multiplicar\n4-División\n5-Salir de la calculadora\n") 
+        opcion = input("Ingrese el numero de su opcion que aparece en el menú: ")
+
+    if opcion == "1":
+        print("\nHAS ELEJIDO SUMAR\n")
+        operación = Calculadora()
+        operación.sumar(int(input("Ingrese el primer numero a sumar: ")), int(input("Ingrese el segundo numero a sumar: ")))
+        input("Enter para continuar...\n")
+    elif opcion == "2":
+        print("\nHAS ELEGIDO RESTAR\n")
+        operación = Calculadora()
+        operación.restar(int(input("Ingrese el primer numero a restar: ")), int(input("Ingrese el segundo numero a restar: "))) 
+        input("Enter para continuar...\n")
+    elif opcion == "3":
+        print("\nHAS ELEGIDO MULTIPLICACIÓN\n")
+        operación = Calculadora()
+        operación.multiplicar(int(input("Ingrese el primer numero a multiplicar: ")), int(input("Ingrese el segundo numero a multiplicar: ")))
+        input("Enter para continuar...\n")
+    elif opcion == "4":
+        print("\nHAS ELEGIDO DIVISIÓN\n")
+        operación = Calculadora()
+        operación.dividir(int(input("Ingrese el primer numero a dividir: ")), int(input("Ingrese el segundo numero a dividir: ")))
+        input("Enter para continuar...\n")
+    elif opcion == "5":
+        break
         
         
-print("BIENVENIDO A LA CALCULADORA, \n1-Sumar\n2-Restar\n3-Multiplicar\n4-División") 
+    else: 
+        print("Esa opcion no existe, intentalo de nuevo\n\n")           
+            
 
-
-opcion = input("Ingrese el numero de su opcion que aprece en el menú: ")
-
-
-while not opcion.isdigit():
-    print("¡ERROR!\n FAVOR INGRESE UN NUMERO ENTERO\n")
-    print("BIENVENDO A LA CALCULADORA, \n1-Sumar\n2-Restar\n3-Multiplicar\n4-División\n") 
-    opcion = input("Ingrese el numero de su opcion que aprece en el menú: ")
-
-if opcion == "1":
-    print("\nHAS ELEJIDO SUMAR\n")
-    operación = Calculadora()
-    operación.sumar(int(input("Ingrese el primer numero a sumar: ")), int(input("Ingrese el segundo numero a sumar: ")))
-elif opcion == "2":
-    print("\nHAS ELEGIDO RESTAR\n")
-    operación = Calculadora()
-    operación.sumar(int(input("Ingrese el primer numero a restar: ")), int(input("Ingrese el segundo numero a restar: "))) 
-elif opcion == "3":
-    print("\nHAS ELEGIDO MULTIPLICACIÓN\n")
-    operación = Calculadora()
-    operación.sumar(int(input("Ingrese el primer numero a multiplicar: ")), int(input("Ingrese el segundo numero a multiplicar: ")))
-elif opcion == "4":
-    print("\nHAS ELEGIDO DIVISIÓN\n")
-    operación = Calculadora()
-    operación.sumar(int(input("Ingrese el primer numero a dividir: ")), int(input("Ingrese el segundo numero a dividir: ")))
-else: 
-    print("Esa opcion no existe, intentalo de nuevo")           
-        
-
+print("Has salido de la calculadora")
